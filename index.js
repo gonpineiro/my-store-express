8bb5c46c23680c19684060a26c36790a31b172c0
Gonzalo <gon.pineiro@gmail.com>
2021-10-12 21:16:21 -0300

    :hammer: Creacion de rutas

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get('/', (req, res) => {
   res.send('Hola mi SRV');
 });
 
+app.get('/new', (req, res) => {
+  res.send('Hola soy un nuevo ENDPOINT');
+});
+
+app.get('/productos', (req, res) => {
+  res.json({
+    name: 'Producto 1',
+    price: 100,
+  });
+});
+
 app.listen(port, () => {
   console.log('Mi port ' + port);
 });
