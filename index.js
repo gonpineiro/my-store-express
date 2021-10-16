7aee59f88b9ad57b32f6bb451b530acd7f88d2cd
Gonzalo <gon.pineiro@gmail.com>
2021-10-16 19:00:40 -0300

    :hammer: Dividir responsabilidades

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const faker = require('faker');
+const routerApi = require('./routes');
 
 const app = express();
 const port = 3000;
@@ -12,54 +12,7 @@ app.get('/new', (req, res) => {
   res.send('Hola soy un nuevo ENDPOINT');
 });
 
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-
-  res.json(products);
-});
-
-app.get('/products/filter', (req, res) => {
-  res.send('Soy un filter');
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'Producto 1',
-    price: 100,
-  });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send('No hay parametros');
-  }
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-});
+routerApi(app);
 
 app.listen(port, () => {
   console.log('Mi port ' + port);
