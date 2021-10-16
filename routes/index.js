57e86dbad58c53a8cd557cc2e3dcf030d0941de8
Gonzalo <gon.pineiro@gmail.com>
2021-10-16 19:10:48 -0300

    :POST: método para crear y middleware

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
+const express = require('express');
+
 const productsRouter = require('./products.router');
 const usersRouter = require('./users.router');
 
 function routerApi(app) {
-  app.use('/products', productsRouter);
-  app.use('/users', usersRouter);
+  const routerV1 = express.Router();
+  const routerV2 = express.Router();
+
+  app.use('/api/v1', routerV1);
+  routerV1.use('/products', productsRouter);
+  routerV1.use('/users', usersRouter);
+
+  app.use('/api/v2', routerV2);
+  routerV2.use('/products', productsRouter);
+  routerV2.use('/users', usersRouter);
 }
 
 module.exports = routerApi;
