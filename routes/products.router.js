5e9a9883ef00d7a2d24712cef5da41c1ccb46388
Gonzalo <gon.pineiro@gmail.com>
2021-10-16 19:16:22 -0300

    PUT, PATCH y DELETE

--- routes/products.router.js
+++ routes/products.router.js
@@ -36,7 +36,27 @@ router.post('/', (req, res) => {
   const body = req.body;
   res.json({
     message: 'created',
-    data: body
+    data: body,
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const body = req.body;
+  res.json({
+    message: 'update-patch',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    message: 'deleted',
+    id,
   });
 });
 
